1b3830bed8862436e474fe9cdc2bc3d8ece5b129
Otunba Marcus <otunbamarcusademola@gmail.com>
2023-03-02 01:35:47 +0100

    updated api

--- src/api/ApiRoutes.js
+++ src/api/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://swift-chat-app-api.onrender.com";
 
 export const addUserApi = `${host}/api/auth/add-user`;
 export const loginUserApi = `${host}/api/auth/login-user`;
